2f928f07eec431972b936bf8ba44c9530ce8ce16
oli-zhao <381942195@qq.com>
2019-09-16 15:41:16 +0800

    travel_06

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe658;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe65c;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display: flex
+  line-height: .86rem
+  background: $bgColor
+  color: #fff
+  .header-left
+    width: .64rem
+    float: left
+    .back-icon
+      display: block
+      text-align: center
+      font-size: .56rem
+  .header-input
+    flex: 1
+    height: .64rem
+    line-height: .64rem
+    margin-top: .12rem
+    margin-left: .2rem
+    padding-left: .12rem
+    background: #fff
+    border-radius: .1rem
+    color: #ccc
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
+    .arrow-icon
+      margin-left: -0.1rem
+</style>
