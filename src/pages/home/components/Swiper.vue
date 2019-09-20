93d477fb8d2832680123a910c84d42cd6bf0fc7c
oli-zhao <381942195@qq.com>
2019-09-20 12:43:57 +0800

    update swiper imgUrl

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -21,16 +21,16 @@ export default {
       },
       swiperList: [{
         id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+        imgUrl: 'https://qdywxs.github.io/travel-images/swiperList01.jpg'
       }, {
         id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+        imgUrl: 'https://qdywxs.github.io/travel-images/swiperList02.jpg'
       }, {
         id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+        imgUrl: 'https://qdywxs.github.io/travel-images/swiperList03.jpg'
       }, {
         id: '0004',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
+        imgUrl: 'https://qdywxs.github.io/travel-images/swiperList04.jpg'
       }]
     }
   }
