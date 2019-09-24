68a4a97ea028c6254ada290bcac3e7a3b30a09e5
oli-zhao <381942195@qq.com>
2019-09-24 21:29:54 +0800

    travel_09

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'https://qdywxs.github.io/travel-images/weekendList01.jpg',
+        title: '北京温泉排行榜',
+        desc: '细数北京温泉，温暖你的冬天'
+      }, {
+        id: '0002',
+        imgUrl: 'https://qdywxs.github.io/travel-images/weekendList02.jpg',
+        title: '北京必游TOP10',
+        desc: '来北京必去的景点非这些地方莫属'
+      }, {
+        id: '0003',
+        imgUrl: 'https://qdywxs.github.io/travel-images/weekendList03.jpg',
+        title: '寻找北京的皇城范儿',
+        desc: '数百年的宫廷庙宇，至今依旧威严霸气'
+      }, {
+        id: '0004',
+        imgUrl: 'https://qdywxs.github.io/travel-images/weekendList04.jpg',
+        title: '学生最爱的博物馆',
+        desc: '周末干嘛？北京很多博物馆已经免费开放啦'
+      }, {
+        id: '0005',
+        imgUrl: 'https://qdywxs.github.io/travel-images/weekendList05.jpg',
+        title: '儿童剧场，孩子的乐园',
+        desc: '带宝贝观看演出，近距离体验艺术的无穷魅力'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  background: #eee
+  line-height: .8rem
+  text-indent: .2rem
+.item-img-wrapper
+  overflow: hidden
+  height: 0
+  padding-bottom: 38%
+  .item-img
+    width: 100%
+.item-info
+  padding: .1rem
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #ccc
+    ellipsis()
+</style>
