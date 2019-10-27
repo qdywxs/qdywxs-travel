8ed3aeed9ba8b8982583803ce3a62d1b01c64f32
oli-zhao <381942195@qq.com>
2019-10-27 22:33:41 +0800

    travel_19

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="https://qdywxs.github.io/travel-images/detail-banner-img.jpg">
+    <div class="banner-info">
+      <div class="banner-title">故宫(AAAAA景区)</div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe64a;</span>
+        3
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    display: flex
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0,0,0, .8))
+    .banner-title
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      height: .32rem
+      line-height: .32rem
+      padding: 0 .4rem
+      margin-top: .14rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      font-size: .24rem
+      color: #fff
+      .banner-icon
+        font-size: .24rem
+</style>
